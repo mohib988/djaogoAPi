a5375a22abb4c3088a0ce551cfde6b1ce6e29a6e
mohib qureshi <qureshimohib819@gmail.com>
2025-04-30 20:24:28 +0500

    added login page

--- AIML/promptApp/urls.py
+++ AIML/promptApp/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from promptApp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
      
     path('',views.CustomPromptView.as_view() ),
-
-    
+    path("login/", views.login_view, name="login"),   
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- AIML/promptApp/views.py
+++ AIML/promptApp/views.py
@@ -7,6 +7,20 @@ from rest_framework.permissions import AllowAny
 from AIML.settings import CONFIG
 # Create your views here.
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")  # Change to your home page URL name
+        else:
+            return render(request, "login.html", {"error": "Invalid credentials"})
+    return render(request, "login.html")
 
 
 class CustomPromptView(APIView):
