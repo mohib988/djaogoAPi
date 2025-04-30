d3023dce7439bfca9d692cdb95353ae010ea682f
mohib qureshi <qureshimohib819@gmail.com>
2025-04-30 22:08:11 +0500

    added whitenoise

--- AIML/AIML/wsgi.py
+++ AIML/AIML/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-from whitenoise.django import DjangoWhiteNoise
+
 
 import os
 
@@ -15,5 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AIML.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
 app=application
