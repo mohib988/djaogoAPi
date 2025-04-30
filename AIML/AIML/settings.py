fb368961255f51a9f2a1662e4bca784bda41bcd3
mohib qureshi <qureshimohib819@gmail.com>
2025-04-30 22:09:16 +0500

    made debugging true

--- AIML/AIML/settings.py
+++ AIML/AIML/settings.py
@@ -34,7 +34,7 @@ CONFIG = {
 SECRET_KEY = 'django-insecure-1*dr3z#1%x96nj3i6+xlh9*t_51yx070o@e4#0j41=h7(sv(t5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*",".vercel.app","https://teal-crumble-6486f8.netlify.app"]
 CSRF_TRUSTED_ORIGINS = [
